52357fdd8058220282ba59afa1fe8ad14c26dc4f
anjin <awsdpr.anji01@gmail.com>
2023-06-22 21:14:16 +0800

    redis installations

--- roboshop-shell1/04-redis.sh
+++ roboshop-shell1/04-redis.sh
@@ -1,15 +1,15 @@
 echo -e "\e[33mInstall rpm\e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y
+yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> /tmp/roboshop.log
 
 echo -e "\e[33m[33mEnable redis 6.2\e[0m"
-yum module enable redis:remi-6.2 -y
+yum module enable redis:remi-6.2 -y &>> /tmp/roboshop.log
 
 echo -e "\e[33mInstall redis\e[0m"
-yum install redis -y
+yum install redis -y &>> /tmp/roboshop.log
 
 echo -e "\e[33m update listen address\e[0m"
-sed -i -e "s/127.0.0.1 to 0.0.0.0"  /etc/redis.conf /etc/redis/redis.conf
+sed -i -e "s/127.0.0.1 to 0.0.0.0"  /etc/redis.conf /etc/redis/redis.conf &>> /tmp/roboshop.log
 
 "\e[33mEnable and restart redis\e[0m"
-systemctl enable redis
-systemctl start redis
+systemctl enable redis &>> /tmp/roboshop.log
+systemctl start redis &>> /tmp/roboshop.log
