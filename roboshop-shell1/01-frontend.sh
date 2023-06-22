6d7ac8628f5e3c50dc3c7c2e06b22bcd31de2fc8
anjin <awsdpr.anji01@gmail.com>
2023-06-22 20:03:31 +0800

    frontend config

--- roboshop-shell1/01-frontend.sh
+++ roboshop-shell1/01-frontend.sh
@@ -6,13 +6,13 @@ echo-e "\${color} Remove the default content\${nocolor}"
 rm -rf /usr/share/nginx/html/* &>>${log}
 
 echo-e "\${color} Download front end content\${nocolor}"
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>${log}
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
 
 echo -e "\${color} Extract the frontend content\${nocolor}"
 cd /usr/share/nginx/html &>>${log}
 unzip /tmp/${component}.zip &>>${log}
-echo "\e[33mUpdate frontend config\e[0m" &>>/tmp/roboshop.log
-#vim /etc/nginx/default.d/roboshop.conf
+echo "\e[33mUpdate frontend config\e[0m"
+cd roboshop.conf /etc/nginx/default.d/roboshop.conf &>>${log}
 echo-e "\${color} Start & Enable Nginx service\${nocolor}"
 systemctl enable nginx &>>${log}
 systemctl readstart nginx &>>${log}
