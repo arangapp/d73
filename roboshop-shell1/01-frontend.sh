9773000e80f565c6f801a4a867498b504869b542
anjin <awsdpr.anji01@gmail.com>
2023-06-22 20:36:43 +0800

    frontend ngnix install

--- roboshop-shell1/01-frontend.sh
+++ roboshop-shell1/01-frontend.sh
@@ -8,11 +8,11 @@ echo -e "\e[33m Download front end content\e[0m"
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip  &>> /tmp/roboshop.log
 
 echo -e "\e[33m Extract the frontend content\e[0m"
-cd /usr/share/nginx/html  &>>/tmp/roboshop.log
-unzip /tmp/frontend.zip &>>/tmp/roboshop.log
-echo -e "\e[33mUpdate frontend config\e[0m" &>> /tmp/roboshop.log
+cd /usr/share/nginx/html  &>> /tmp/roboshop.log
+unzip /tmp/frontend.zip &>> /tmp/roboshop.log
+echo -e "\e[33mUpdate frontend config\e[0m" &>>/tmp/roboshop.log
 #vim /etc/nginx/default.d/roboshop.conf
-cp /home/centos/d73/roboshop-shell1 /etc/nginx/default.d/roboshop.conf
+cp /home/centos/d73/roboshop-shell1//roboshop.conf /etc/nginx/default.d/roboshop.conf &>>/tmp/roboshop.log
 echo -e "\e[33m Start & Enable Nginx service\e[0m"
 systemctl enable nginx &>> /tmp/roboshop.log
 systemctl restart nginx &>> /tmp/roboshop.log
