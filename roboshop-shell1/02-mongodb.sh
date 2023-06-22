6a78ed6404b0e8f2f74c719f8bdb904f3e8c91ea
anjin <awsdpr.anji01@gmail.com>
2023-06-22 23:27:41 +0800

    rabbitmq installations updates

--- roboshop-shell1/02-mongodb.sh
+++ roboshop-shell1/02-mongodb.sh
@@ -1,11 +1,14 @@
 echo -e "\e[33m Setup the MongoDB repo file \e[0m"
-cd mongo.repo /etc/yum.repos.d/mongo.repo &>> /tmp/roboshop.log
+cp /home/centos/d73/roboshop-shell1/mongo.repo /etc/yum.repos.d/mongo.repo &>> /tmp/roboshop.log
+
 echo -e "\e[33m Install MongoDB \e[0m"
 yum install mongodb-org -y &>> /tmp/roboshop.log
+
 echo -e "\e[33m Update listen address \e[0m"
 sed -i -e "s/27.0.0.1 to 0.0.0.0cc" /etc/mongod.conf &>> /tmp/roboshop.log
+
 echo -e "\e[33m Enable & Restart MongoDB \e[0m"
-systemctl enable mongod &>>/tmp/roboshop.log &>> /tmp/roboshop.log
-systemctl restart mongod  &>>/tmp/roboshop.log &>> /tmp/roboshop.log
+systemctl enable mongod &>> /tmp/roboshop.log
+systemctl restart mongod  &>> /tmp/roboshop.log
 
 
