16a2bf6b71b0c16b9ba7d9706d298908aae08155
anjin <awsdpr.anji01@gmail.com>
2023-06-23 00:01:25 +0800

    Payment installations upate

--- roboshop-shell1/02-mongodb.sh
+++ roboshop-shell1/02-mongodb.sh
@@ -5,7 +5,7 @@ echo -e "\e[33m Install MongoDB \e[0m"
 yum install mongodb-org -y &>> /tmp/roboshop.log
 
 echo -e "\e[33m Update listen address \e[0m"
-sed -i -e "s/27.0.0.1 to 0.0.0.0cc" /etc/mongod.conf &>> /tmp/roboshop.log
+sed -i -e "s/127.0.0.1/0.0.0.0" /etc/mongod.conf &>> /tmp/roboshop.log
 
 echo -e "\e[33m Enable & Restart MongoDB \e[0m"
 systemctl enable mongod &>> /tmp/roboshop.log
