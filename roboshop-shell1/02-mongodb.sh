d344fae388fe2e6b51abe4d0bc2b45c049410d10
anjin <awsdpr.anji01@gmail.com>
2023-06-22 20:46:24 +0800

    frontend ngnix install

--- roboshop-shell1/02-mongodb.sh
+++ roboshop-shell1/02-mongodb.sh
@@ -1,10 +1,10 @@
-echo -e "e\[33m Setup the MongoDB repo file \e[0m"
+echo -e "\e[33m Setup the MongoDB repo file \e[0m"
 cd mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
-echo -e "e\[33m Install MongoDB \e[0m"
+echo -e "\e[33m Install MongoDB \e[0m"
 yum install mongodb-org -y &>>/tmp/roboshop.log
-echo -e "e\[33m Update listen address \e[0m"
+echo -e "\e[33m Update listen address \e[0m"
 sed -i -e "s/27.0.0.1 to 0.0.0.0cc" /etc/mongod.conf &>>/tmp/roboshop.log
-echo -e "e\[33m Enable & Restart MongoDB \e[0m"
+echo -e "\e[33m Enable & Restart MongoDB \e[0m"
 
 systemctl enable mongod &>>/tmp/roboshop.log
 systemctl start mongod  &>>/tmp/roboshop.log
