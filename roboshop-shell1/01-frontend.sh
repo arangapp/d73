1ffe6680262e0e51c82798621f553edde317b1c8
anjin <awsdpr.anji01@gmail.com>
2023-06-22 19:32:02 +0800

    frontend installation

--- roboshop-shell1/01-frontend.sh
+++ roboshop-shell1/01-frontend.sh
@@ -1,20 +1,20 @@
 source common.sh
-echo -e "\${color} Install ngnix \${nocolor}" &>>${log}
-yum install nginx -y
+echo -e "\${color} Install ngnix \${nocolor}"
+yum install nginx -y &>>${log}
 
-echo-e "\${color} Remove the default content\${nocolor}" &>>${log}
-rm -rf /usr/share/nginx/html/*
+echo-e "\${color} Remove the default content\${nocolor}"
+rm -rf /usr/share/nginx/html/* &>>${log}
 
-echo-e "\${color} Download front end content\${nocolor}" &>>${log}
-curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip
+echo-e "\${color} Download front end content\${nocolor}"
+curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>${log}
 
-echo -e "\${color} Extract the frontend content\${nocolor}" &>>${log}
-cd /usr/share/nginx/html
-unzip /tmp/${component}.zip
+echo -e "\${color} Extract the frontend content\${nocolor}"
+cd /usr/share/nginx/html &>>${log}
+unzip /tmp/${component}.zip &>>${log}
 #echo "\e[33mcreate ngnix proxy configuration\e[0m" &>>/tmp/roboshop.log
 #vim /etc/nginx/default.d/roboshop.conf
-echo-e "\${color} Start & Enable Nginx service\${nocolor}" &>>${log}
-systemctl enable nginx
-systemctl readstart nginx
+echo-e "\${color} Start & Enable Nginx service\${nocolor}"
+systemctl enable nginx &>>${log}
+systemctl readstart nginx &>>${log}
 
 
