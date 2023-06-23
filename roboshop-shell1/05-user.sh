103f54c58f1051c8d5f8470716657d6d1e16b28f
anjin <awsdpr.anji01@gmail.com>
2023-06-24 00:11:44 +0800

    user update

--- roboshop-shell1/05-user.sh
+++ roboshop-shell1/05-user.sh
@@ -6,6 +6,8 @@ yum install nodejs -y &>> /tmp/roboshop.log
 echo -e "\e[33m Add Application User \e[0m"
 useradd roboshop &>> /tmp/roboshop.log
 
+echo -e "\e[33m remove App directory \e[0m"
+rm -rf /app &>> /tmp/roboshop.log
 echo -e "\e[33m Setup App directory \e[0m"
 mkdir /app &>> /tmp/roboshop.log
 
