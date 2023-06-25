208b607e095fbf23fb3990e2bedf65710c403375
anjin <awsdpr.anji01@gmail.com>
2023-06-25 20:08:23 +0800

    catalogue update

--- roboshop-shell1/03-catalogue.sh
+++ roboshop-shell1/03-catalogue.sh
@@ -1,73 +1,16 @@
-VALIDATE(){
-if [ $1 -ne 0 ]; then
-	echo "$2 ... FAILURE"
-	exit 1
-else
-	echo "$2 ... SUCCESS"
-fi
-}
-
-echo -e "\e[33mSetup NodeJS repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>> /tmp/roboshop.log
-VALIDATE $? "Setup NodeJS repo"
-
-echo -e "\e[33mInstall Node Js\e[0m"
-yum install nodejs -y &>> /tmp/roboshop.log
-VALIDATE $? "Install nodeJs"
-
-echo -e "\e[33mremove app user\e[0m"
-rm -rf /home/roboshop &>> /tmp/roboshop.log
-VALIDATE $? "remove App user"
-
-cho -e "\e[33madd app user\e[0m"
-useradd roboshop &>> /tmp/roboshop.log
-VALIDATE $? "Add App user"
-
-echo -e "\e[33msetup an app directory\e[0m"
-rm -rf /app &>> /tmp/roboshop.log
-mkdir /app &>> /tmp/roboshop.log
-VALIDATE $? "Setup App directory"
-
-echo -e "\e[33mDownload the application code to created app directory\e[0m"
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>> /tmp/roboshop.log
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+yum install nodejs -y
+useradd roboshop
+mkdir /app
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
 cd /app
-VALIDATE $? "download application code"
-
-echo -e "\e[33mExtract code \e[0m"
-unzip /tmp/catalogue.zip &>> /tmp/roboshop.log
+unzip /tmp/catalogue.zip
 cd /app
-
-VALIDATE $? "extract appcode"
-
-echo -e "\e[33mInstall npm\e[0m"
-npm install &>> /tmp/roboshop.log
-
-VALIDATE $? "Install npm"
-
-echo -e "\e[33mSetup SystemD Catalogue Service\e[0m"
-cp /home/centos/d73/roboshop-shell1/catalogue.service /etc/systemd/system/catalogue.service &>> /tmp/roboshop.log
-VALIDATE $? "Setup systemd"
-
-echo -e "\e[33mLoad the service\e[0m"
-systemctl daemon-reload &>> /tmp/roboshop.log
-VALIDATE $? "load service"
-
-echo -e "\e[33mEnable & Restart server\e[0m"
-systemctl enable catalogue &>> /tmp/roboshop.log
-systemctl restart catalogue &>> /tmp/roboshop.log
-VALIDATE $? "enable &restart service"
-
-echo -e "\e[33m setup mongodb repo\e[0m"
+npm install
+cp cp /home/centos/d73/roboshop-shell1/catalogue.service   /etc/systemd/system/catalogue.service
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl start catalogue
 cp /home/centos/d73/roboshop-shell1/mongo.repo  /etc/yum.repos.d/mongo.repo &>> /tmp/roboshop.log
-VALIDATE $? "Setup mongodb repo"
-
-echo -e "\e[33mInstall mongodb-client\e[0m"
-yum install mongodb-org-shell -y &>> /tmp/roboshop.log
-VALIDATE $? "Install mongodb client"
-
-echo -e "\e[33mLoad Schema\e[0m"
-mongo --host mongodb-dev.adevlearn.shop </app/schema/catalogue.js &>> /tmp/roboshop.log
-VALIDATE $? "load schema"
-
-
-
+yum install mongodb-org-shell -y
+mongo --host mongodb-dev.adevlearn.shop </app/schema/catalogue.js
