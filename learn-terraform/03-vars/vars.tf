839077d4871242e2112c0a368d4202e248d1d0c5
anjin <awsdpr.anji01@gmail.com>
2023-06-27 23:30:34 +0800

    terraform variables

--- learn-terraform/03-vars/vars.tf
+++ learn-terraform/03-vars/vars.tf
@@ -16,3 +16,53 @@ output "sample1" {
 output "sample-ext" {
   value = "Value of sample & sample1 - ${var.sample} - ${var.sample1}"
 }
+
+# Variable data type
+# string
+# number
+# boolean
+
+#Variable types in Ansible
+
+# plain key ,list ,Map/dict
+
+#In terraform
+#1.Plain
+#2.List
+#3.Map
+
+# plain
+variable "course" {
+  default = "DevOps training"
+}
+
+variable "courses" {
+  default = [
+   "aws"
+   "terraform"
+    "Ansible"
+  ]
+}
+
+variable "course_details" {
+  aws = {
+    "ec2"
+    "s3"
+  }
+  ansible = {
+    "vars"
+    "functions"
+    "modules"
+  }
+}
+
+output "course" {
+  value = "var.course"
+}
+output "courses" {
+  value = "var.courses"
+}
+
+output "course_details" {
+  value = "var.course_details"
+}
