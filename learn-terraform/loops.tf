e137c3deff4d1f0232146dbb0b9d8bd4788d3750
anjin <awsdpr.anji01@gmail.com>
2023-07-10 04:43:52 +0800

    terraform variables

---
+++ learn-terraform/loops.tf
@@ -0,0 +1,30 @@
+resource "aws_instance" "web" {
+  for_each = var.instances
+  ami           = "ami-a1b2c3d4"
+  instance_type = lookup(each.value, "instance_type", "t2.micro")
+
+  tags = {
+    Name = each.key
+  }
+}
+
+data "aws_ami" "example" {
+  owner = ["973714476881"]
+  most_recent = true
+  name_regex = "Centos-8-DevOps-Practice"
+}
+
+variable "instances" {
+
+  default = {
+    frontend ={
+      name = "frontend"
+    }
+    catalogue ={
+      name = "catalogue"
+    }
+    cart ={
+      name = "cart"
+    }
+  }
+}
