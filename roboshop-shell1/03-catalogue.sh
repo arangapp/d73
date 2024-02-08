e282ac338f346017212d5be84c956088a7416998
anjin <awsdpr.anji01@gmail.com>
2024-02-08 18:56:40 +0800

    catalogue changes

--- roboshop-shell1/03-catalogue.sh
+++ roboshop-shell1/03-catalogue.sh
@@ -1,17 +1,17 @@
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
-yum install nodejs -y
-useradd roboshop
-mkdir /app
-curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
-cd /app
-unzip /tmp/catalogue.zip
-cd /app
-npm install
-cp cp /home/centos/d73/roboshop-shell1/catalogue.service   /etc/systemd/system/catalogue.service
-cp /home/centos/d73/roboshop-shell1/mongo.repo  /etc/yum.repos.d/mongo.repo &>> /tmp/roboshop.log
-yum install mongodb-org-shell -y
-mongo --host mongodb-dev.adevlearn.shop </app/schema/catalogue.js
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+yum install nodejs -y &>>/tmp/roboshop.log
+useradd roboshop &>>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
+cd /app &>> /tmp/roboshop.log &>>/tmp/roboshop.log
+unzip /tmp/catalogue.zip &>> /tmp/roboshop.log &>>/tmp/roboshop.log
+cd /app &>> /tmp/roboshop.log &>>/tmp/roboshop.log
+npm install &>> /tmp/roboshop.log &>>/tmp/roboshop.log
+cp cp /home/centos/d73/roboshop-shell1/catalogue.service   /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp /home/centos/d73/roboshop-shell1/mongo.repo  /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
+mongo --host mongodb-dev.adevlearn.shop </app/schema/catalogue.js &>>/tmp/roboshop.log
 
-systemctl daemon-reload
-systemctl enable catalogue
-systemctl restart catalogue
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable catalogue &>>/tmp/roboshop.log
+systemctl restart catalogue &>>/tmp/roboshop.log
