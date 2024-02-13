8c262f1a86572dc37c2893f6626c52ecd653b53a
anjin <awsdpr.anji01@gmail.com>
2024-02-13 23:29:59 +0800

    Mysql changes

--- roboshop-shell1/07-mysql.sh
+++ roboshop-shell1/07-mysql.sh
@@ -1,18 +1,26 @@
+source common.sh
+
 echo -e "\e[33m disable MySQL 8 version \e[0m"
-yum module disable mysql -y &>> /tmp/roboshop.log
+dnf module disable mysql -y  &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33m Setup the MySQL5.7 repo file \e[0m"
-cp cp /home/centos/d73/roboshop-shell1/mysql.repo /etc/yum.repos.d/mysql.repo &>> /tmp/roboshop.log
+cp cp /home/centos/d73/roboshop-shell1/mysql.repo /etc/yum.repos.d/mysql.repo &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33m Install MySQL Server \e[0m"
-yum install mysql-community-server -y &>> /tmp/roboshop.log
+dnf install mysql-community-server -y &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33m enable & restart MqSql \e[0m"
-systemctl enable mysqld &>> /tmp/roboshop.log
-systemctl restart mysqld &>> /tmp/roboshop.log
+systemctl enable mysqld &>>/tmp/roboshop.log
+systemctl restart mysqld &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33m change the default root password \e[0m"
-mysql_secure_installation --set-root-pass RoboShop@1 &>> /tmp/roboshop.log
+mysql_secure_installation --set-root-pass RoboShop@1 &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33m check the new password working or not \e[0m"
-mysql -uroot -pRoboShop@1 &>> /tmp/roboshop.log
+mysql -uroot -pRoboShop@1 &>>/tmp/roboshop.log
+VALIDATE $?
