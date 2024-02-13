c2eadc2ab860c9366b4174eb08b5572221efbd45
anjin <awsdpr.anji01@gmail.com>
2024-02-13 22:32:20 +0800

    redis changes

--- roboshop-shell1/04-redis.sh
+++ roboshop-shell1/04-redis.sh
@@ -1,15 +1,22 @@
-echo -e "\e[33mInstall rpm\e[0m"
-yum install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>> /tmp/roboshop.log
+source common.sh
 
-echo -e "\e[33mEnable redis 6.2\e[0m"
-yum module enable redis:remi-6.2 -y &>> /tmp/roboshop.log
+echo -e "\e[33m Install rpm \e[0m"
+dnf install https://rpms.remirepo.net/enterprise/remi-release-8.rpm -y &>>/tmp/roboshop.log
+VALIDATE $?
+
+echo -e "\e[33m Enable redis 6.2 \e[0m"
+dnf module enable redis:remi-6.2 -y &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33mInstall redis\e[0m"
-yum install redis -y &>> /tmp/roboshop.log
+dnf install redis -y  &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33m update listen address\e[0m"
-sed -i -e 's/127.0.0.1/0.0.0.0/g'  /etc/redis.conf /etc/redis/redis.conf &>> /tmp/roboshop.log
+sed -i -e 's/127.0.0.1/0.0.0.0/g' /etc/redis.conf /etc/redis/redis.conf &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33mEnable and restart redis\e[0m"
-systemctl enable redis &>> /tmp/roboshop.log
-systemctl start redis &>> /tmp/roboshop.log
+systemctl enable redis &>>/tmp/roboshop.log
+systemctl start redis &>>/tmp/roboshop.log
+VALIDATE $?
