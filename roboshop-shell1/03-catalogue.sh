592419f0164d2fc529df6d45d3eec61be3a09101
anjin <awsdpr.anji01@gmail.com>
2024-02-13 18:09:49 +0800

    catalogue changes

--- roboshop-shell1/03-catalogue.sh
+++ roboshop-shell1/03-catalogue.sh
@@ -1,28 +1,44 @@
+source common.sh
 echo -e "\e[33m  \e[0m"
 curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
+VALIDATE $?
 
-echo -e "\e[33m  \e[0m"
+echo -e "\e[33m Install nodejs \e[0m"
 yum install nodejs -y &>>/tmp/roboshop.log
+VALIDATE $?
 
-echo -e "\e[33m  \e[0m"
+echo -e "\e[33m setup an app directory \e[0m"
 useradd roboshop &>>/tmp/roboshop.log
 mkdir /app &>>/tmp/roboshop.log
-echo -e "\e[33m  \e[0m"
+VALIDATE $?
+
+echo -e "\e[33m  Download the application code \e[0m"
 curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip &>>/tmp/roboshop.log
 cd /app &>> /tmp/roboshop.log &>>/tmp/roboshop.log
+VALIDATE $?
 unzip /tmp/catalogue.zip &>> /tmp/roboshop.log &>>/tmp/roboshop.log
 cd /app &>> /tmp/roboshop.log &>>/tmp/roboshop.log
+VALIDATE $?
 
-echo -e "\e[33m  \e[0m"
+echo -e "\e[33m Install npm\e[0m"
 npm install &>> /tmp/roboshop.log &>>/tmp/roboshop.log
-echo -e "\e[33m  \e[0m"
-cp cp /home/centos/d73/roboshop-shell1/catalogue.service   /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
-cp /home/centos/d73/roboshop-shell1/mongo.repo  /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+VALIDATE $?
 
-echo -e "\e[33m  \e[0m"
+echo -e "\e[33m Setup SystemD Catalogue Service \e[0m"
+cp /home/centos/d73/roboshop-shell1/catalogue.service  /etc/systemd/system/catalogue.service &>>/tmp/roboshop.log
+cp /home/centos/d73/roboshop-shell1/mongo.repo  /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
+VALIDATE $?
+
+echo -e "\e[33m setup MongoDB repo  \e[0m"
 yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 mongo --host mongodb-dev.adevlearn.shop </app/schema/catalogue.js &>>/tmp/roboshop.log
-echo -e "\e[33m  \e[0m"
+VALIDATE $?
+
+echo -e "\e[33m Load the service \e[0m"
 systemctl daemon-reload &>>/tmp/roboshop.log
+VALIDATE $?
+
+echo -e "\e[33m enable & Start the service \e[0m"
 systemctl enable catalogue &>>/tmp/roboshop.log
 systemctl restart catalogue &>>/tmp/roboshop.log
+VALIDATE $?
