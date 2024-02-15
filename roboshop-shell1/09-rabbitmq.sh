3c53f6d38fe0be7f2a1b094b4dd38fabbe52230e
anjin <awsdpr.anji01@gmail.com>
2024-02-15 20:33:23 +0800

    rabbitmq changes

--- roboshop-shell1/09-rabbitmq.sh
+++ roboshop-shell1/09-rabbitmq.sh
@@ -1,16 +1,23 @@
+source common.sh
+
 echo -e "\e[33m configure yum repos \e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>> /tmp/roboshop.log
+curl -s https://packagecloud.io/install/repositories/rabbitmq/erlang/script.rpm.sh | bash &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33m Configure YUM Repos for RabbitMQ \e[0m"
-curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>> /tmp/roboshop.log
+curl -s https://packagecloud.io/install/repositories/rabbitmq/rabbitmq-server/script.rpm.sh | bash &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33m Install RabbitMQ \e[0m"
-yum install rabbitmq-server -y &>> /tmp/roboshop.log
+dnf install rabbitmq-server -y &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33m create a user for the application \e[0m"
 rabbitmqctl add_user roboshop roboshop123 &>> /tmp/roboshop.log
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>> /tmp/roboshop.log
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>/tmp/roboshop.log
+VALIDATE $?
 
 echo -e "\e[33m enable & start rabbitmq \e[0m"
-systemctl enable rabbitmq-server  &>> /tmp/roboshop.log
-systemctl restart rabbitmq-server  &>> /tmp/roboshop.log
+systemctl enable rabbitmq-server  &>>/tmp/roboshop.log
+systemctl restart rabbitmq-server  &>>/tmp/roboshop.log
+VALIDATE $?
