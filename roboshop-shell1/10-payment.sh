df9902add6c9392af9ba3d8b962774055b900908
anjin <awsdpr.anji01@gmail.com>
2024-02-15 21:03:46 +0800

    rabbitmq changes

--- roboshop-shell1/10-payment.sh
+++ roboshop-shell1/10-payment.sh
@@ -1,31 +1,31 @@
 source common.sh
-log="&>>/tmp/roboshop.log"
+log="/tmp/roboshop.log"
 
 echo -e "\e[33m Install python3.6 \e[0m"
-dnf install python36 gcc python3-devel -y ${log}
+dnf install python36 gcc python3-devel -y &>>${log}
 
 echo -e "\e[33m add application user \e[0m"
-useradd roboshop ${log}
+useradd roboshop &>>${log}
 
 echo -e "\e[33m Set an app directory \e[0m"
-mkdir /app ${log}
+mkdir /app &>>${log}
 
 echo -e "\e[33m Download the Application code \e[0m"
-curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip ${log}
-cd /app ${log}
-unzip /tmp/payment.zip ${log}
+curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>${log}
+cd /app &>>${log}
+unzip /tmp/payment.zip &>>${log}
 
 echo -e "\e[33m Download dependencies \e[0m"
-cd /app ${log}
-pip3.6 install -r requirements.txt ${log}
+cd /app &>>${log}
+pip3.6 install -r requirements.txt &>>${log}
 
 echo -e "\e[33mSetup SystemD Payment Service \e[0m"
-cp /home/centos/d73/roboshop-shell1/payment.service  /etc/systemd/system/payment.service ${log}
+cp /home/centos/d73/roboshop-shell1/payment.service  /etc/systemd/system/payment.service &>>${log}
 
 echo -e "\e[33m Load the service \e[0m"
-systemctl daemon-reload ${log}
+systemctl daemon-reload &>>${log}
 
 echo -e "\e[33m Enable & restart service \e[0m"
-systemctl enable payment ${log}
-systemctl restart payment ${log}
+systemctl enable payment &>>${log}
+systemctl restart payment &>>${log}
 
