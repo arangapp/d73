161637d397fbe3f87cc0cfdede11638e38149d2c
anjin <awsdpr.anji01@gmail.com>
2024-02-15 21:05:08 +0800

    rabbitmq changes

--- roboshop-shell1/10-payment.sh
+++ roboshop-shell1/10-payment.sh
@@ -3,29 +3,36 @@ log="/tmp/roboshop.log"
 
 echo -e "\e[33m Install python3.6 \e[0m"
 dnf install python36 gcc python3-devel -y &>>${log}
+VALIDATE $?
 
 echo -e "\e[33m add application user \e[0m"
 useradd roboshop &>>${log}
+VALIDATE $?
 
 echo -e "\e[33m Set an app directory \e[0m"
 mkdir /app &>>${log}
+VALIDATE $?
 
 echo -e "\e[33m Download the Application code \e[0m"
 curl -L -o /tmp/payment.zip https://roboshop-artifacts.s3.amazonaws.com/payment.zip &>>${log}
 cd /app &>>${log}
 unzip /tmp/payment.zip &>>${log}
+VALIDATE $?
 
 echo -e "\e[33m Download dependencies \e[0m"
 cd /app &>>${log}
 pip3.6 install -r requirements.txt &>>${log}
+VALIDATE $?
 
 echo -e "\e[33mSetup SystemD Payment Service \e[0m"
 cp /home/centos/d73/roboshop-shell1/payment.service  /etc/systemd/system/payment.service &>>${log}
+VALIDATE $?
 
 echo -e "\e[33m Load the service \e[0m"
 systemctl daemon-reload &>>${log}
+VALIDATE $?
 
 echo -e "\e[33m Enable & restart service \e[0m"
 systemctl enable payment &>>${log}
 systemctl restart payment &>>${log}
-
+VALIDATE $?
