4d3b73560fb528a2a239090df3eb114b602c1a65
anjin <awsdpr.anji01@gmail.com>
2024-02-16 15:25:45 +0800

    common script update

--- roboshop-shell1/05-user.sh
+++ roboshop-shell1/05-user.sh
@@ -50,23 +50,23 @@ echo -e "\e[33m Setup SystemD ${component} Service \e[0m"
 cp /home/centos/d73/roboshop-shell1/${component}.service  /etc/systemd/system/${component}.service &>>${log}
 VALIDATE $?
 
-echo -e "\e[33 Load the service\e[0m"
+echo -e "\e[33 Load the service \e[0m"
 systemctl daemon-reload &>> ${log}
 VALIDATE $?
 
 
-echo -e "\e[33 setup mongodb repo\e[0m"
+echo -e "\e[33 setup mongodb repo \e[0m"
 cd /home/centos/d73/roboshop-shell1/mongo.repo /etc/yum.repos.d/mongo.repo &>>${log}
 
-echo -e "\e[33 Install mongodb\e[0m"
+echo -e "\e[33 Install mongodb \e[0m"
 yum install mongodb-org-shell -y &>>${log}
 VALIDATE $?
 
-echo -e "\e[33 Load Schema\e[0m"
+echo -e "\e[33 Load Schema \e[0m"
 mongo --host mongodb-dev.adevlearn.shop </app/schema/${component}.js &>>${log}
 VALIDATE $?
 
 echo -e "\e[33 Enable & Restart service \e[0m"
 systemctl enable ${component} &>>${log}
-systemctl restart ${component}  &>>${log}
+systemctl restart ${component} &>>${log}
 VALIDATE $?
