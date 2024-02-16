9d7f9d03239d1fa39bfe83da745c6a4f7c1ed778
anjin <awsdpr.anji01@gmail.com>
2024-02-16 22:28:26 +0800

    common script update

--- roboshop-shell1/06-cart.sh
+++ roboshop-shell1/06-cart.sh
@@ -1,49 +1,7 @@
 source common.sh
 component=cart
 
-echo -e "\e[33m module disable nodejs \e[0m"
-dnf module disable nodejs -y &>> ${log}
-VALIDATE $?
-
-echo -e "\e[33m module enable nodejs \e[0m"
-dnf module enable nodejs:18 -y &>>${log}
-VALIDATE $?
-
-echo -e "\e[33m Install NodeJs \e[0m"
-dnf install nodejs -y &>>${log}
-VALIDATE $?
-
-echo -e "\e[33m add ${component} \e[0m"
-id roboshop &>>${log}
-userdel roboshop &>>${log}
-useradd roboshop &>>${log}
-VALIDATE $?
-
-echo -e "\e[33m remove add directory \e[0m"
-rm -rf /app
-VALIDATE $?
-
-echo -e "\e[33m add directory \e[0m"
-mkdir /app &>>${log}
-VALIDATE $?
-
-
-echo -e "\e[33m  Download the application code \e[0m"
-curl -L -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip  &>>${log}
-cd /app &>>${log}
-unzip /tmp/${component}.zip &>>${log}
-VALIDATE $?
-
-
-echo -e "\e[33m Install npm\e[0m"
-cd /app
-npm install &>>${log}
-VALIDATE $?
-
-echo -e "\e[33m Setup SystemD ${component} Service \e[0m"
-cp /home/centos/d73/roboshop-shell1/${component}.service  /etc/systemd/system/${component}.service &>>${log}
-cp /home/centos/d73/roboshop-shell1/mongo.repo  /etc/yum.repos.d/mongo.repo &>>${log}
-VALIDATE $?
+nodejs
 
 cd /app
 echo -e "\e[33m Setup SystemD ${component} Service \e[0m"
