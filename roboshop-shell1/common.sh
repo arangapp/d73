2de3dccc0fff5662c0e067ec81dbde66ca6428fb
anjin <awsdpr.anji01@gmail.com>
2024-02-16 15:00:48 +0800

    common script update

--- roboshop-shell1/common.sh
+++ roboshop-shell1/common.sh
@@ -1,5 +1,7 @@
 
-log="/tmp/roboshop.log"
+log=/tmp/roboshop.log
+
+USERID=$(id -u)
 
 VALIDATE(){
 if [ $1 -ne 0 ]; then
@@ -9,3 +11,8 @@ else
 	echo "$2 ... SUCCESS"
 fi
 }
+
+if [ $USERID -ne 0 ]; then
+	echo "You need to be root user to execute this script"
+	exit 1
+fi
