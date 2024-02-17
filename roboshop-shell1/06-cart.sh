745fba181f83db3a9d19d604610fdc5e51a4c29d
anjin <awsdpr.anji01@gmail.com>
2024-02-17 15:45:05 +0800

    all service update

--- roboshop-shell1/06-cart.sh
+++ roboshop-shell1/06-cart.sh
@@ -1,15 +1,16 @@
 source common.sh
 component=cart
 nodejs
+
 echo -e "${color} Setup SystemD ${component} Service ${nocolor}"
 cp /home/centos/d73/roboshop-shell1/${component}.service  /etc/systemd/system/${component}.service &>>${log}
 VALIDATE $?
 
-echo -e "\e[33 Load the service ${nocolor}"
+echo -e "${color} Load the service ${nocolor}"
 systemctl daemon-reload &>>${log}
 VALIDATE $?
 
-echo -e "\e[33 Enable and restart service ${nocolor}"
+echo -e "${color} Enable and restart service ${nocolor}"
 systemctl enable ${component} &>>${log}
 systemctl start ${component}  &>>${log}
 VALIDATE $?
