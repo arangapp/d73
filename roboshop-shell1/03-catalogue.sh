4f9658d7f5c1670a8100f36fdccbc88e7699e448
anjin <awsdpr.anji01@gmail.com>
2024-02-17 15:28:54 +0800

    all service update

--- roboshop-shell1/03-catalogue.sh
+++ roboshop-shell1/03-catalogue.sh
@@ -3,14 +3,6 @@ component=catalogue
 
 nodejs
 
-echo -e "${color} setup MongoDB repo  ${nocolor}"
-dnf install mongodb-org-shell -y &>>${log}
-VALIDATE $?
-
-echo -e "${color} Load Master Data of the List of products ${nocolor}"
-mongo --host mongodb-dev.adevlearn.shop </app/schema/${component}.js &>>${log}
-VALIDATE $?
-
 echo -e "${color} Load the service ${nocolor}"
 systemctl daemon-reload &>>${log}
 VALIDATE $?
@@ -19,3 +11,12 @@ echo -e "${color} enable & Start the service ${nocolor}"
 systemctl enable ${component} &>>${log}
 systemctl restart ${component} &>>${log}
 VALIDATE $?
+
+echo -e "${color} setup MongoDB repo  ${nocolor}"
+dnf install mongodb-org-shell -y &>>${log}
+VALIDATE $?
+
+echo -e "${color} Load Master Data of the List of products ${nocolor}"
+mongo --host mongodb-dev.adevlearn.shop </app/schema/${component}.js &>>${log}
+VALIDATE $?
+
