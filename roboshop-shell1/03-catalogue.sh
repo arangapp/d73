3146df62414f897e7e9838d01b59573c46cbd333
anjin <awsdpr.anji01@gmail.com>
2024-02-17 15:33:46 +0800

    all service update

--- roboshop-shell1/03-catalogue.sh
+++ roboshop-shell1/03-catalogue.sh
@@ -3,6 +3,11 @@ component=catalogue
 
 nodejs
 
+echo -e "${color} Setup SystemD ${component} Service ${nocolor}"
+cp /home/centos/d73/roboshop-shell1/${component}.service  /etc/systemd/system/${component}.service &>>${log}
+cp /home/centos/d73/roboshop-shell1/mongo.repo  /etc/yum.repos.d/mongo.repo &>>${log}
+VALIDATE $?
+
 echo -e "${color} Load the service ${nocolor}"
 systemctl daemon-reload &>>${log}
 VALIDATE $?
